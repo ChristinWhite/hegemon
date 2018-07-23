f1ca3128a786c2b6d8c0e8a68190a2fa0473f53b
Chris White <chris@christopherdwhite.com>
2018-07-23 13:16:41 -0600

    Add initial script file

---
+++ hegemon.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#
+# Hegemon
+# hegemon.sh
+#
+# Setup a macOS development environment with Ansible.
+#
+# Created by Chris White on 7/23/2018
+# License: MIT License / https://opensource.org/licenses/MIT
+#
